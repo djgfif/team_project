83848cbc29e75cd8102e424bab2992f68bee7e2c
djgfif <100983794+djgfif@users.noreply.github.com>
2023-06-03 15:29:57 +0900

    origin

--- teamProject/MyShoppingMall/assets/js/index.js
+++ teamProject/MyShoppingMall/assets/js/index.js
@@ -1,7 +1,8 @@
 window.onload = function() {
-  const loginYN = JSON.parse(localStorage.getItem('loginYN'));
+  var loginYN = JSON.parse(localStorage.getItem('loginYN'));
 
   if (loginYN) {
+    // User is logged in
     const userData = JSON.parse(localStorage.getItem('userData'));
     const textForm = document.getElementById("text-area");
     const loginForm = document.getElementById("login-area");
@@ -10,7 +11,6 @@ window.onload = function() {
 
     loginForm.style.display = "none";
 
-
     const icon = document.createElement("i");
     icon.classList.add("bi", "bi-person", "me-2", "display-6"); // Add "display-1" class to make the icon larger
 
@@ -18,12 +18,22 @@ window.onload = function() {
     username.textContent = userData.name + "님";
     username.classList.add("fw-bold");
 
+    const logout = document.createElement("button");
+    logout.textContent = "로그아웃";
+    logout.addEventListener("click", function() {
+      // Perform logout actions
+      localStorage.setItem('loginYN', false);
+      location.reload(); // Refresh the page after logout
+    });
+
     userFrom.appendChild(icon);
     userFrom.appendChild(username);
+    userFrom.appendChild(logout);
 
     textForm.appendChild(userFrom);
   } else {
+    // User is not logged in
+    const loginForm = document.getElementById("login-area");
     loginForm.style.display = "inline";
-    userFrom.style.display = "none";
   }
 };
