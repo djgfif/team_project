b82a9ca04edb5da74a50b469670d28d98fec9d57
djgfif <100983794+djgfif@users.noreply.github.com>
2023-06-08 12:18:40 +0900

    origin

---
+++ teamProject/MyShoppingMall/assets/js/photo.js
@@ -0,0 +1,23 @@
+// 웹페이지 로드 이벤트 등록 
+window.addEventListener('load', ()=>{
+    const grid = new Isotope('section', {
+       // isotope 옵션 지정
+        itemSelector: 'article',
+        columnWidth: 'article',
+        transitionDuration: '0.5s'
+    });
+    const btns = document.querySelectorAll('main ul li');
+    for(let e of btns) {
+        e.addEventListener('click', e=>{
+           const sort = 
+                 e.currentTarget.querySelector('a').getAttribute('sort');
+            grid.arrange({
+                filter: sort
+            });
+            for(let item of btns) {
+                item.classList.remove('on'); 
+            }
+            e.currentTarget.classList.add('on'); // 선택한 버튼의 배경색 변경
+        });
+    }
+});
