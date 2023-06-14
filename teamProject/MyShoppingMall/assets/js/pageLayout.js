1cd78ced730776084a442b218f5f0ea6a19e411c
djgfif <100983794+djgfif@users.noreply.github.com>
2023-06-14 23:38:04 +0900

    origin

--- teamProject/MyShoppingMall/assets/js/pageLayout.js
+++ teamProject/MyShoppingMall/assets/js/pageLayout.js
@@ -61,13 +61,12 @@ function getHeaderContent() {
 
 function getFooterContent() {
   return `
-    <div class="w3-xlarge w3-section">
-      <i class="fa fa-facebook-official w3-hover-opacity"></i>
-      <i class="fa fa-instagram w3-hover-opacity"></i>
-      <i class="fa fa-snapchat w3-hover-opacity"></i>
-      <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-      <i class="fa fa-twitter w3-hover-opacity"></i>
-      <i class="fa fa-linkedin w3-hover-opacity"></i>
+    <div class="w3-xlarge w3-section"></a>
+      <a href=""><i class="fa fa-facebook-official w3-hover-opacity"></i></a>
+      <a href=""><i class="fa fa-instagram w3-hover-opacity"></i></a>
+      <a href=""><a href="https://github.com/djgfif/team_project"><i class="fa fa-github w3-hover-opacity"></i></a>
+      <a href=""><i class="fa fa-twitter w3-hover-opacity"></i></a>
+      <a href=""><i class="fa fa-linkedin w3-hover-opacity"></i></a>
     </div>
     <p>Powered by <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-text-green">w3.css</a></p>
   `;
